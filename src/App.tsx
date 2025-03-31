f2d1a8558d6d544420fed5be0fb0abf5ca2aaf30
carson1024 <carsongale1024@gmail.com>
2025-03-31 04:47:58 -0700

    Remove contact

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,10 @@ import Toastr from "components/toastr";
 const App = () => {
   return (
     <>
-    {
-      <pre>
-        Hi, Kornelijus,<br/>
-        Hope you're doing well!<br/>
-
-        I'm available to start working from this week. <br/>
-        But now, my Upwork account got unexpectedly blocked, and I'm currently in touch with their support team to sort it out. It might take a bit of time to get it back.<br/>
-
-        In the meantime, I suggest we continue working outside of Upwork—maybe through Telegram (@dreamer2575) or Slack if that works better for project management.<br/>
-
-        Let me know what you think.<br/>
-
-        Best,<br/>
-        Anton<br/>
-      </pre>
-    }
-      {/* <Toastr />
+      <Toastr />
       <Routes>
         <Route path="/*" element={<MainLayout />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 };
